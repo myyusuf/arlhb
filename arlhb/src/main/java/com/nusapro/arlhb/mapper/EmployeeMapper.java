fe1f57f4a2239002365806e707a9a2abc26e689e
myyusuf <myyusuf1911@gmail.com>
2017-01-24 12:33:31 +0700

    Add xml mapper

--- arlhb/src/main/java/com/nusapro/arlhb/mapper/EmployeeMapper.java
+++ arlhb/src/main/java/com/nusapro/arlhb/mapper/EmployeeMapper.java
@@ -3,21 +3,12 @@ package com.nusapro.arlhb.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
 
 import com.nusapro.arlhb.model.Employee;
 
 @Mapper
 public interface EmployeeMapper {
 	
-	@Results({
-		@Result(property = "employeeId", column = "employeeId"),
-        @Result(property = "firstName", column = "first_name"),
-        @Result(property = "lastName", column = "last_name")
-      })
-	@Select("SELECT * FROM employee")
 	List<Employee> findAll();
 
 }
