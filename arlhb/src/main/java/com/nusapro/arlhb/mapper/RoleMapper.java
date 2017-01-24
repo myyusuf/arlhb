f8ce00c39379dab7a69faa3f1641f838ca24186f
myyusuf <myyusuf1911@gmail.com>
2017-01-24 12:59:08 +0700

    Add role mapper xml file

--- arlhb/src/main/java/com/nusapro/arlhb/mapper/RoleMapper.java
+++ arlhb/src/main/java/com/nusapro/arlhb/mapper/RoleMapper.java
@@ -1,41 +1,19 @@
 package com.nusapro.arlhb.mapper;
 
 import java.util.List;
-
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-
 import com.nusapro.arlhb.model.Role;
 
 @Mapper
 public interface RoleMapper {
 	
-	@Results({
-        @Result(property = "roleId", column = "role_id"),
-        @Result(property = "roleName", column = "role_name")
-      })
-	@Select("SELECT * FROM role WHERE role_id = #{roleId}")
-    Role findById(@Param("roleId") int roleId);
+    Role findById(int roleId);
 	
-	@Results({
-        @Result(property = "roleId", column = "role_id"),
-        @Result(property = "roleName", column = "role_name")
-      })
-	@Select("SELECT * FROM role")
 	List<Role> findAll();
 	
-	@Insert("INSERT into role(role_id, role_name) VALUES(#{roleId}, #{roleName})")
 	void create(Role role);
 	
-	@Update("UPDATE role SET role_name=#{roleName} WHERE role_id = #{roleId}")
 	void update(Role role);
-	
-	@Delete("DELETE FROM role WHERE role_id = #{roleId}")
-	void delete(int id);
+
+	void delete(int roleId);
 }
